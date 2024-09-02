f5bd6d3a4dadb2e8309fd2f245a6b1932a071050
artizsq <timurchikkrash@gmail.com>
2024-09-02 17:56:58 +0300

    Удалил лишние импорты

--- exc.py
+++ exc.py
@@ -1,6 +1,5 @@
 from aiogram.filters import BaseFilter
 from data.requests import get_chat, create_chat, get_queue, add_queue, delete_queue, delete_chat
-import asyncio
 
 class IsChat(BaseFilter):
     async def __call__(self, message):
